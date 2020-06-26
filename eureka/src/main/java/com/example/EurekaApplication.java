bbb3de7d4254b73e9808608b5bcd1497e4ab4457
Lysander686 <Lysander@example.com>
2020-06-26 20:13:51 +0800

    init

---
+++ eureka/src/main/java/com/example/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueVideoApplication.class, args);
+    }
+
+}
