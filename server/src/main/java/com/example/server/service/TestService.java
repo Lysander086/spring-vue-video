78b7116b345fd5885352125e212f4392669c53c8
Lysander686 <Lysander@example.com>
2020-06-27 16:27:28 +0800

    example使用示例, 能够使用example快速完成crud

--- server/src/main/java/com/example/server/service/TestService.java
+++ server/src/main/java/com/example/server/service/TestService.java
@@ -2,6 +2,7 @@ package com.example.server.service;
 
 
 import com.example.server.domain.Test;
+import com.example.server.domain.TestExample;
 import com.example.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,11 @@ public class TestService {
 
 //    for test
     public List<Test> list() {
-        return testMapper.selectByExample(null);
+        TestExample example = new TestExample();
+        example.createCriteria().andIdEqualTo("1"); // where 语句
+        example.setOrderByClause("id asc"); // order by
+
+        return testMapper.selectByExample(example);
     }
 
 
