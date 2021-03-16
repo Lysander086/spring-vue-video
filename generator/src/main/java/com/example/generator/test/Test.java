51f307ac9a6d3eb8507e0fbe29e9dd9a0a925f33
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-03-16 12:56:18 +0800

    update readme

--- generator/src/main/java/com/example/generator/test/Test.java
+++ generator/src/main/java/com/example/generator/test/Test.java
@@ -2,7 +2,7 @@ package com.example.generator.test;
 
 public class Test {
     /**
-    * ID
-    */
+     * ID
+     */
     private String id;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-与教程使用的mysql版本有差异, 使用的是mysql8
- 
- (参见application.properties)
+# project info
+- 本项目使用的是mysql8
+- (参见application.properties)
